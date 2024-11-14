7b4e162a5bf3873193c3e12d640cecd07c3aee76
ashrafmui <ashrafmuhaiminul253@gmail.com>
2024-11-14 14:36:10 -0500

    matplot rendering of sleep data

--- lifelens_backend/sleepProcessing.py
+++ lifelens_backend/sleepProcessing.py
@@ -1,33 +1,9 @@
 import pandas as pandas;
+import matplotlib.pyplot as plt
 
-sleepData = pandas.read_csv('sleeps.csv');
+#Processing and cleaning data
+sleepData = pandas.read_csv('sleeps.csv', nrows = 24)
+filtered_Data = sleepData[sleepData['Nap'] != True]
+filtered_Data = filtered_Data.iloc[1:]
 
-print(sleepData.head())
-
-# sleepData.fillna()
-
-sleepData['Sleep Duration'] = pandas.to_datetime(sleepData['Wake onset']) - pandas.to_datetime(sleepData['Sleep onset']);
-
-print(sleepData['Sleep Duration']);
-
-optimal_light_sleep = 0.55 * sleepData['Sleep Duration'];
-optimal_deep_sleep = 0.20 * sleepData['Sleep Duration'];
-optimal_rem_sleep = 0.25 * sleepData['Sleep Duration'];
-# optimal_total_sleep = 0.25 * sleepData['Sleep Duration'];
-
-# optimal_light_sleep_hours = optimal_light_sleep.dt.components['hours']
-# optimal_light_sleep_minutes = optimal_light_sleep.dt.components['minutes']
-
-optimal_light_sleep_str = optimal_light_sleep.dt.floor('min').astype(str).str[-8:-3]
-
-print(optimal_light_sleep);
-print(optimal_deep_sleep);
-print(optimal_rem_sleep);
-
-sleepData['Light Sleep Difference'] = sleepData['Light sleep duration (min)'] - optimal_light_sleep;
-sleepData['Deep Sleep Difference'] = sleepData['Deep (SWS) duration (min)'] - optimal_deep_sleep;
-sleepData['REM Sleep Difference'] = sleepData['REM duration (min)'] - optimal_rem_sleep;
-
-print(sleepData['Light Sleep Difference']);
-print(sleepData['Deep Sleep Difference']);
-print(sleepData['REM Sleep Difference']);
+filtered_Data.to_csv('sleepData.csv', index=False)
